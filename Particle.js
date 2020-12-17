b61d06e1d8512b570d6e714459b6625d2eb79131
Rishon-P <73733774+Rishon-P@users.noreply.github.com>
2020-12-17 19:06:42 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,18 @@
+class Particle{
+    constructor(x,y,radius){
+        this.body = Bodies.circle(x,y,radius)
+        this.color = color(random(0,255),random(0,255),random(0,255))
+        this.radius = radius
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position
+        var angle = this.body.angle
+        push();
+        translate(pos.x,pos.y)
+        rotate(angle)
+        ellipseMode(CENTER)
+        ellipse(0,0,this.radius,this.radius)
+        pop();
+    }
+}
